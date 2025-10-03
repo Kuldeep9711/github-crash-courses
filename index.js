6593cbc225c7b4bc1016bc859ea8ab365c90d069
Kuldeep Sainigit config --global user.name Kuldeep <k72125642@gmail.com>
2025-10-03 23:41:11 +0530

    first

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const age = 24;
-const name = 'Kuldeep saini'
-const dob = '23/10/2001'
+const name = 'Kuldeep Sainii'
+const dob = '23/90/2001'
 
 function sayName() {
     console.log('My Name is ' + gaurav2)
